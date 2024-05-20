c55c51ccb3af8f5f45754b9c1eb90b46d86eb3cd
Valdezin <ops@vldz.cf>
2024-05-20 20:56:34 +0800

    improve training.py

--- training.py
+++ training.py
@@ -9,6 +9,7 @@ def load_images_from_folder(folder):
     images = []
     labels = []
     label_names = []
+    image_paths = []
     for label in os.listdir(folder):
         label_path = os.path.join(folder, label)
         if os.path.isdir(label_path):
@@ -19,9 +20,10 @@ def load_images_from_folder(folder):
                 if image is not None:
                     images.append(image)
                     labels.append(label)
-    return images, labels, label_names
+                    image_paths.append(image_path)
+    return images, labels, label_names, image_paths
 
-def extract_features(images):
+def extract_features_and_clean(images, image_paths):
     features = []
     winSize = (128, 128)
     blockSize = (16, 16)
@@ -31,12 +33,13 @@ def extract_features(images):
     hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins)
     i = 0
 
-    for image in images:
+    for image, image_path in zip(images, image_paths):
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
         if len(faces) == 0:
-            print(f"No faces detected in image: {i}")
+            print(f"No faces detected in image: {image_path}")
+            os.remove(image_path)
         else:
             for (x, y, w, h) in faces:
                 face_img = image[y:y+h, x:x+w]
@@ -47,20 +50,12 @@ def extract_features(images):
         i += 1
     return features
 
-    # for image in images:
-    #     # Resize image to a fixed size (e.g., 64x64) for consistency
-    #     image = cv2.resize(image, (128, 128))
-    #     # Extract HOG features
-    #     feature = hog.compute(image).flatten()
-    #     features.append(feature)
-    # return features
-
 # Load images and labels
 folder_path = 'faces'
-images, labels, label_names = load_images_from_folder(folder_path)
+images, labels, label_names, image_paths = load_images_from_folder(folder_path)
 
-# Extract features from images
-features = extract_features(images)
+# Extract features from images and delete images without faces
+features = extract_features_and_clean(images, image_paths)
 
 # Encode labels into numeric values
 label_encoder = LabelEncoder()
